91dbcf9b4bdd63026e66b40d6345a3ba0029bf5a
unknown <okorojiebube2@gmail.com>
2022-07-05 01:02:19 -0600

    Add association

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,3 +1,6 @@
 class Business < ApplicationRecord
   belongs_to :author
+  has_many :users, dependent: :destroy
+  has_many :category_businesses, dependent: :destroy
+  has_many :categories, through: :category_businesses
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,6 @@
 class Category < ApplicationRecord
   belongs_to :author
+  has_many :users, dependent: :destroy
+  has_many :category_businesses, dependent: :destroy
+  has_many :businesses, through: :category_businesses
 end
