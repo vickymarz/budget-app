bed36b7d93f588b5bbfc63287becb3b7416d3274
unknown <okorojiebube2@gmail.com>
2022-07-05 00:52:11 -0600

    Create business database

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_05_062822) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_05_064451) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "businesses", force: :cascade do |t|
+    t.string "name"
+    t.string "icon"
+    t.bigint "author_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_businesses_on_author_id"
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -41,5 +50,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_05_062822) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "businesses", "users", column: "author_id"
   add_foreign_key "categories", "users", column: "author_id"
 end
