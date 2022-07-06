2561aacb9c6cc09c264709addc4e09e197ca9481
unknown <okorojiebube2@gmail.com>
2022-07-06 12:36:10 -0600

    Add signup functionality

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_05_064451) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_06_071012) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum"
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
 
   create_table "businesses", force: :cascade do |t|
     t.string "name"
@@ -21,6 +49,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_05_064451) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["author_id"], name: "index_businesses_on_author_id"
+  end
+
+  create_table "businesses_categories", id: false, force: :cascade do |t|
+    t.bigint "category_id", null: false
+    t.bigint "business_id", null: false
+    t.index ["business_id", "category_id"], name: "index_businesses_categories_on_business_id_and_category_id"
+    t.index ["category_id", "business_id"], name: "index_businesses_categories_on_category_id_and_business_id"
   end
 
   create_table "categories", force: :cascade do |t|
@@ -50,6 +85,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_05_064451) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "businesses", "users", column: "author_id"
   add_foreign_key "categories", "users", column: "author_id"
 end
