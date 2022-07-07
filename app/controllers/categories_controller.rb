368292ec82d3a1567b21a573f8304c866c8512e3
unknown <okorojiebube2@gmail.com>
2022-07-07 01:31:55 -0600

    Create category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,7 +4,25 @@ class CategoriesController < ApplicationController
   end
   
   def new
-    @category = Category.new
+    @category = current_user.categories.new
+  end
+  
+  def create
+    @category = current_user.categories.new(category_params)
+
+    if @category.save
+      flash[:success] = "Category successfully created"
+      redirect_to categories_path
+    else
+      flash[:error] = "Something went wrong"
+      render 'new'
+    end
+  end
+
+  def category_params
+    my_category = params.require(:category).permit(:name, :icon)
+    my_category[:user] = current_user
+    my_category
   end
   
 end
