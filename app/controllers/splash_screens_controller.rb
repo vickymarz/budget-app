409665b86cb838cab41c5915770240e4f9d5045a
unknown <okorojiebube2@gmail.com>
2022-07-08 07:56:01 -0600

    Add business model test

--- app/controllers/splash_screens_controller.rb
+++ app/controllers/splash_screens_controller.rb
@@ -6,8 +6,6 @@ class SplashScreensController < ApplicationController
   private
 
   def authenticate!
-    if user_signed_in?
-      redirect_to categories_path
-    end
+    redirect_to categories_path if user_signed_in?
   end
 end
